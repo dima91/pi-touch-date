70a509032bb80fc586d184d33c5749e927aa3e8b
Luca <dima91@live.it>
2026-05-16 00:03:02 +0200

    Enables Wi-Fi network discovery
    Initializes the Wi-Fi manager to discover available networks. Sets up a background task to periodically scan for networks, populating an observable collection for UI display. Includes a scanning indicator for user feedback.

--- PiTouchDate/ViewModels/Overlays/WifiSettingsOverlayViewModel.cs
+++ PiTouchDate/ViewModels/Overlays/WifiSettingsOverlayViewModel.cs
@@ -3,13 +3,66 @@ using System.Reactive.Linq;
 using ReactiveUI;
 using Avalonia.Interactivity;
 using PiTouchDate.ViewModels;
+using SharpWifiManager;
+using Avalonia.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace PiTouchDate.Overlays;
 
 public class WifiSettingsViewModel : ViewModelBase
 {
+    private WifiManager _wifiManager;
+
+    private bool _scanning = false;
+    public bool Scanning {
+        get => _scanning;
+        set => this.RaiseAndSetIfChanged(ref _scanning, value);
+    }
+
+    public ObservableCollection<WifiNetwork> Networks { get; } = new();
+
+    
     public WifiSettingsViewModel()
     {
         Console.WriteLine("Overlay constructor!");
+
+        _wifiManager = new WifiManager();
+
+        _ = StartScanningLoop();
+    }
+
+
+    private async Task StartScanningLoop()
+    {
+        while (true)
+        {
+            Scanning = true;
+            try {
+                await RefreshNetworks();
+            }
+            catch {}
+            finally
+            {
+                Scanning = false;
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+        }
+    }
+
+    private async Task RefreshNetworks ()
+    {
+        IReadOnlyList<WifiNetwork> currentNetworks = await _wifiManager.GetNetworksAsync();
+
+        await Dispatcher.UIThread.InvokeAsync(() =>
+        {
+            Networks.Clear();
+            foreach (var network in currentNetworks)
+            {
+                Networks.Add(network);
+            }
+        });
     }
 }
