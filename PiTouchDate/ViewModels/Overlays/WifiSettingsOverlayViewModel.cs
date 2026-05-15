cc038156850977b58cab621c8e06383bcdba2bca
Luca <dima91@live.it>
2026-05-15 23:25:48 +0200

    Adds Wi-Fi settings overlay
    Introduces the basic structure for a Wi-Fi settings overlay, including its view and view model. This lays the groundwork for future network configuration functionality.
    
    Reduces the default padding for generic overlays for a more compact appearance.

---
+++ PiTouchDate/ViewModels/Overlays/WifiSettingsOverlayViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reactive.Linq;
+using ReactiveUI;
+using Avalonia.Interactivity;
+using PiTouchDate.ViewModels;
+
+namespace PiTouchDate.Overlays;
+
+public class WifiSettingsViewModel : ViewModelBase
+{
+    public WifiSettingsViewModel()
+    {
+        Console.WriteLine("Overlay constructor!");
+    }
+}
---
+++ PiTouchDate/Views/Overlays/WifiSettingsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PiTouchDate.Overlays;
+
+public partial class WifiSettingsView : UserControl
+{
+    public WifiSettingsView()
+    {
+        InitializeComponent();
+    }
+}
